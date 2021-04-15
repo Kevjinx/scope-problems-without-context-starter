a4837daff655a7d7d31bc7624f7cbeac5230c456
Kevin Mei <kevjinx@gmail.com>
2021-04-15 13:47:20 -0700

    4-6

--- problems/05-arrow-mirror-array.js
+++ problems/05-arrow-mirror-array.js
@@ -12,6 +12,30 @@ arrowMirrorArray(['a', 'b', 'c', 'd']); // => [ 'a', 'b', 'c', 'd', 'd', 'c', 'b
 
 // your code here
 
+const arrowMirrorArray = arr => {
+
+  console.log([arr.concat(arr.reverse()))];
+  return [arr.concat(arr.reverse())];
+
+  // let reverse = [];
+  // for (let i = arr.length - 1; i >= 0; i--) {
+  //   const element = arr[i];
+  //   reverse.push(element);
+  // }
+  // arr = arr.concat(reverse)
+  // console.log(arr);
+  // return arr;
+
+}
+
+
+arrowMirrorArray([1,2,3]); // => [ 1, 2, 3, 3, 2, 1 ]
+arrowMirrorArray(['a', 'b', 'c', 'd']); // => [ 'a', 'b', 'c', 'd', 'd', 'c', 'b', 'a' ]
+
+
+
+
+
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
--- problems/06-closure-sandwiches.js
+++ problems/06-closure-sandwiches.js
@@ -1,6 +1,6 @@
 /***********************************************************************
 Let's practice writing closures by creating a function named `sandwichMaker()`.
-This function will return another function that will accept a string to add 
+This function will return another function that will accept a string to add
 to the sandwich order (which will start off with a default "tomato" ingredient),
 separating each incoming ingredient with "and".
 
@@ -18,6 +18,28 @@ sandwich2("pb") // => "One sandwich with tomato and pb"
 ***********************************************************************/
 
 // your code here!
+
+const sandwichMaker = function() {
+  let food = "One sandwich with tomato";
+  return function(string) {
+    return food += ' and ' + string
+  }
+}
+
+
+
+
+
+
+
+
+let sandwich = sandwichMaker(); // => returns a function
+
+console.log(sandwich("spinach")) // => "One sandwich with console.log(tomato and spinach"
+console.log(sandwich("jelly")) // => "One sandwich with tomato console.log(and spinach and jelly"
+console.log(sandwich("bread")) // => "One sandwich with tomato console.log(and spinach and jelly and bread"
+let sandwich2 = sandwichMaker(); // => returns a console.log(function
+console.log(sandwich2("pb")) // => "One sandwich with tomato and pb"
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
