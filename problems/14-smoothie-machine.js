f6103620c9122ec4df961854bdf3c28934dbdcfd
Kevin Mei <kevjinx@gmail.com>
2021-04-16 08:17:18 -0700

    review 14

--- problems/14-smoothie-machine.js
+++ problems/14-smoothie-machine.js
@@ -23,6 +23,40 @@ console.log(smoothie2("pineapple"));
 ***********************************************************************/
 
 // your code here
+//?????   when assigning smoothiemachine to a variable, it doesn't invoking smoothiemachine?????
+//how to return
+const smoothieMachine = () => {
+  let order = 'I/m having a smoothie with ';
+  let firstCount = 0;
+  return (...item) => {
+    if (firstCount === 0) {
+      order = order + item
+      firstCount ++;
+      return order;
+    }
+    item.forEach(item => {
+      order = order + ' and ' + item;
+    })
+    return order;
+  }
+}
+
+
+
+let smoothie1 = smoothieMachine();
+
+console.log(smoothie1("milk"));
+// prints "I'm having a smoothie with milk"
+console.log(smoothie1("kale", "spinach"));
+// prints "I'm having a smoothie with milk and kale and spinach"
+console.log(smoothie1("honey", "pears", "berries"));
+// prints "I'm having a smoothie with milk and kale and spinach and honey and pears and berries"
+
+let smoothie2 = smoothieMachine("apples", "bananas", "berries");
+console.log(smoothie2("pineapple"));
+// prints "I'm having a smoothie with apples and bananas and berries and pineapple"
+
+
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
